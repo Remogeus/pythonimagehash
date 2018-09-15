32ccdcf45f566bb1f19757104055701d2169bf52
Remogeus <Remogeus@gmail.com>
2018-09-15 21:43:24 +0200

    edited code style according to PEP 8

--- src/imagehash.py
+++ src/imagehash.py
@@ -34,19 +34,22 @@ import os
 
 local_folder = os.path.dirname(os.path.abspath(__file__))
 
-def imgPrepare(filename):
-    """ imgPrepare(filename)
-        
+
+def img_prepare(filename):
+    """ img_prepare(filename)
+
         Returns a numpy array created from grayscale 32x32 version of the
         image. Grayscaling is achieved by using the LA (grayscale with
-        alpha) mode from PIL and is done for simplifying the subsequent 
+        alpha) mode from PIL and is done for simplifying the subsequent
         calculations (same with downscaling the images to 32x32 pixels)
 
-        Parameters: @filename - string, name of the image that should be returned
+        Parameters: @filename - string, name of the image that should be
+                    returned
         Returns:    numpy array
     """
-    image = Image.open(filename).convert('LA').resize((32,32))
+    image = Image.open(filename).convert('LA').resize((32, 32))
     return np.asarray(image)
+
 
 def dct2d(arg):
     """ dct2d(arg)
@@ -60,30 +63,32 @@ def dct2d(arg):
     """
     return dct(dct(arg.T, norm='ortho').T, norm='ortho')
 
-def imgHashCreate(filename):
-    """imgHashCreate(filename)
+
+def img_hash_create(filename):
+    """img_hash_create(filename)
 
         Returns a hash created from an image using the pHash algorithm
         (perceptual hashing) using DCT.
 
-        Parameters: @filename - string, name of the image from which 
+        Parameters: @filename - string, name of the image from which
                     the hash is calculated
         Returns:    string - 64 bit number
     """
-    imgArr = imgPrepare(filename)
-    imgArr = np.squeeze(imgArr[:, :, :1].transpose(2,0,1))
-    imgDCT = dct2d(imgArr)
-    imgDCT = imgDCT[:8,:8]
-    imgDCTAvg = np.mean(imgDCT[1:,:])
-    imgHash = [0]*64
-    imgDCT = imgDCT.flatten()
-    for x in range(0,64):
-        if imgDCT[x] > imgDCTAvg:
-            imgHash[x] = 1
-    return ''.join(map(str,imgHash))
-    
-def hamming2(s1,s2):
-    """ hamming2(s1, s2)
+    img_arr = img_prepare(filename)
+    img_arr = np.squeeze(img_arr[:, :, :1].transpose(2, 0, 1))
+    img_dct = dct2d(img_arr)
+    img_dct = img_dct[:8, :8]
+    img_dct_avg = np.mean(img_dct[1:, :])
+    img_hash = [0]*64
+    img_dct = img_dct.flatten()
+    for x in range(0, 64):
+        if img_dct[x] > img_dct_avg:
+            img_hash[x] = 1
+    return ''.join(map(str, img_hash))
+
+
+def hamming_bin(s1, s2):
+    """ hamming_bin(s1, s2)
         Returns Hamming distance calculated from 2 binary numbers. Hamming
         distance measures the minimum amount of substitutions required to
         change one string to another string of equal lenght.
@@ -93,35 +98,36 @@ def hamming2(s1,s2):
         Returns:    integer - Hamming distance
     """
     assert len(s1) == len(s2)
-    return sum(c1 != c2 for c1, c2 in zip(s1,s2))
+    return sum(c1 != c2 for c1, c2 in zip(s1, s2))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog='imagehash',
         description='The script creates hashes from two images and then '
-            'calculates their Hamming distance to measure diff'
-            'erence between them'
+                    'calculates their Hamming distance to measure diff'
+                    'erence between them'
             )
     parser.add_argument(
-        'firstImage',
-        metavar = 'firstImage',
-        nargs = 1,
-        help = 'first image to be processed'
+        'first_image',
+        metavar='first_image',
+        nargs=1,
+        help='first image to be processed'
             )
     parser.add_argument(
-        'secndImage',
-        metavar = 'secndImage',
-        nargs = 1,
-        help = 'second image to be processed'
+        'second_image',
+        metavar='second_image',
+        nargs=1,
+        help='second image to be processed'
             )
-    
-    args = parser.parse_args()
-    imgHash1 = imgHashCreate(os.path.join(local_folder, ''.join(args.firstImage)))
-    imgHash2 = imgHashCreate(os.path.join(local_folder, ''.join(args.secndImage)))
-    print(''.join(args.firstImage), end = "\t", flush=True)
-    print(hex(int(imgHash1,2)), end = "\t", flush=True)
-    print(''.join(args.secndImage), end = "\t", flush=True)
-    print(hex(int(imgHash2,2)), end = "\t", flush=True)
-    print(hamming2(imgHash1, imgHash2))
-    
 
+    args = parser.parse_args()
+    img_hash_1 = img_hash_create(
+            os.path.join(local_folder, ''.join(args.first_image)))
+    img_hash_2 = img_hash_create(
+            os.path.join(local_folder, ''.join(args.second_image)))
+    print(''.join(args.first_image), end="\t", flush=True)
+    print(hex(int(img_hash_1, 2)), end="\t", flush=True)
+    print(''.join(args.second_image), end="\t", flush=True)
+    print(hex(int(img_hash_2, 2)), end="\t", flush=True)
+    print(hamming_bin(img_hash_1, img_hash_2))
